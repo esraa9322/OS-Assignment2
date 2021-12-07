0b080e1f70ca22a99c9b1b10fa369ec3e586d2bd
Esraa Yazid <60916510+esraa9322@users.noreply.github.com>
2021-12-07 13:31:01 +0200

    fefe

--- src/Network.java
+++ src/Network.java
@@ -7,7 +7,7 @@ public class Network {
     private static ArrayList <Device> TC_lines;
     private static Router router;
     private static Scanner inputScanner = new Scanner(System.in);
-    public static String stringBuffer;
+    public static String stringBuffer = "";
 
     private static Device createDevice()
     {
@@ -18,8 +18,6 @@ public class Network {
     }
 
     public static void main(String[] args) {
-        Scanner input;
-
         //N max number of connections a router can hold
         int N,TC;
         System.out.println("What is the number of WI-FI connections ?");
@@ -32,12 +30,11 @@ public class Network {
         inputScanner.nextLine();
         TC_lines = new ArrayList<>(TC);
 
-        for (int i =1; i<TC;i++ )
-        {
+        for (int i =0; i<TC;i++ )
             TC_lines.add(createDevice());
-        }
 
-        System.out.println(stringBuffer);
+        for(int i = 0; i < TC;i++)
+            TC_lines.get(i).start();
 
     }
 }
@@ -80,41 +77,31 @@ class Router {
         {
             deviecsSemaphore.waitf();
             connectedDevices.add(device);
-            Network.stringBuffer += "Connection " + connectedDevices.size() + ": " + device.name + "Occupied\n";
+            System.out.println("Connection " + connectedDevices.size() + ": " + device.name + " Occupied");
         }
 
         public void releaseConnection(Device device)
         {
             String r = device.logout();
             connectedDevices.remove(device);
+            System.out.println("Connection " + (connectedDevices.size() + 1) + ": " + r);
             deviecsSemaphore.signal();
-            Network.stringBuffer += "Connection " + connectedDevices.size() + ": " + r + "\n";
         }
 
 }
 
 class Device extends Thread {
 
-    private Random rand = new Random();
     private Router router;
 
     @Override
-    public synchronized void run() {
+    public void run() {
         super.run();
-        try {
-            wait(rand.nextInt(500));
             connect();
-            wait(rand.nextInt(500));
             router.occupyConnection(this);
-            wait(rand.nextInt(500));
             login();
-            wait(rand.nextInt(500));
             perform_online_activity();
-            wait(rand.nextInt(500));
             router.releaseConnection(this);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     String name;
@@ -124,24 +111,24 @@ class Device extends Thread {
         this.name = name;
         this.type = type;
         this.router = router;
-        run();
     }
 
     public void connect()
     {
-        Network.stringBuffer += "(" +  name + ") (" + type  + ") arrived\n";
+        System.out.println("(" +  name + ") (" + type  + ") arrived");
     }
 
    public void login(){
-        Network.stringBuffer += name + "login\n";
+       System.out.println(name + " login");
 
    }
 
    public void perform_online_activity(){
-        Network.stringBuffer += name+ "performs online activity \n";
+       System.out.println(name+ " performs online activity");
    }
+
    public String logout(){
-        return name + "Logged out\n";
+        return name + " Logged out";
    }
 
 }
